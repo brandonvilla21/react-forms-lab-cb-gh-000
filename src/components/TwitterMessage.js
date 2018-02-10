6596d9dd23971fba8dc551ea610705e7eca98321
Brandon Villa <bornofos@gmail.com>
2018-02-10 00:09:13 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,20 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: 0
+    };
   }
-
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={this.handleChange}/>
       </div>
     );
   }
